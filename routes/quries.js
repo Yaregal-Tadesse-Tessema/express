6cd35b32a3399436e9b53348ddccc58f510d2dc9
yayasoles <yayasoles@gmail.com>
2024-04-12 09:07:50 +0300

    node module

--- routes/quries.js
+++ routes/quries.js
@@ -6,12 +6,12 @@ queryrouter.get("/", (req, res) => {
 });
 queryrouter.get("/users", async (req, res) => {
   try {
-    // console.log("ddddddddddddddddddddd", process.env.DB);
-    // console.log("ddddddddddddddddddddd", process.env.DB_PORT);
-    // console.log("ddddddddddddddddddddd", process.env.DB_TYPE);
-    // console.log("ddddddddddddddddddddd", process.env.DB_USER);
-    // console.log("ddddddddddddddddddddd", process.env.DB_PASSWORD);
-    // console.log("ddddddddddddddddddddd", process.env.DEV_DB);
+    console.log("ddddddddddddddddddddd", process.env.DB);
+    console.log("ddddddddddddddddddddd", process.env.DB_PORT);
+    console.log("ddddddddddddddddddddd", process.env.DB_TYPE);
+    console.log("ddddddddddddddddddddd", process.env.DB_USER);
+    console.log("ddddddddddddddddddddd", process.env.DB_PASSWORD);
+    console.log("ddddddddddddddddddddd", process.env.DEV_DB);
     const userRepository = getConnection().getRepository("User");
     const result = await userRepository.find();
     const data = { result: result, total: 10 };
